de2e3ad4e840f3ea46a6d037bbec2b307c1f1f53
Sarika <Sarika@Sarika-PC>
2017-11-03 01:16:54 +0530

    fgttttt

--- BookStore/src/main/java/com/example/demo/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/demo/BookStoreApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.example")
 public class BookStoreApplication {
 	public static void main(String[] args) {
+		System.out.println("ffutugjfj");
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 }
