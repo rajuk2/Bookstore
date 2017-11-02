2cdb009e53e555490cb32f923caff77514c3aed1
Sarika <Sarika@Sarika-PC>
2017-11-02 23:46:38 +0530

    added one line

--- BookStore/src/main/java/com/example/demo/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/demo/BookStoreApplication.java
@@ -14,6 +14,7 @@ public class BookStoreApplication {
 	
 	
 	public static void main(String[] args) {
+		System.out.println("inside main");
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 }
